94f5ac2fef24ca32e0cdbfab123879d14e085fdd
Cristian Viterales <cviterales@cooperativabatan.com.ar>
2021-06-04 19:01:11 -0300

    fixed data Info component

--- src/components/Molecules/Info/Info.js
+++ src/components/Molecules/Info/Info.js
@@ -11,37 +11,38 @@ const Info = ({ subAccData, title }) => {
         <div className={styles.innerHeader}>
           <h4 className={styles.cardTitle}>
             <FontAwesomeIcon icon={faUserCircle} color="#D7B644" style={{ marginRight: "0.5rem" }} /> {title}
-            {subAccData?.info[0]?.id_sub_account}
+            {subAccData?.info?.id_sub_account}
           </h4>
-          <h5 className={subAccData?.balance === 0 ? styles.balance_false : styles.balance_true}>
+          {subAccData?.balance && <h5 className={subAccData?.balance === 0 ? styles.balance_false : styles.balance_true}>
             {subAccData?.balance ? "Deuda: " + subAccData.balance : "Sin Deuda"}
-          </h5>
+          </h5>}
+
         </div>
         <div className={styles.cardContent}>
           <p>
-            <span className={styles.boldText}>Razon social:</span> {subAccData?.info[0]?.account_name}
+            <span className={styles.boldText}>Razon social:</span> {subAccData?.info?.account_name}
           </p>
           <p>
             <span className={styles.boldText}>Domicilio: </span>
-            {subAccData?.info[0]?.address}, {subAccData?.info[0]?.region_name}
+            {subAccData?.info?.address}, {subAccData?.info?.region_name}
           </p>
           <p>
             <span className={styles.boldText}>DNI: </span>
-            {subAccData?.info[0]?.doc_number}
+            {subAccData?.info?.doc_number}
           </p>
-          {subAccData?.phones.length > 0 ? (
+          {subAccData?.phones?.length > 0 && (
             subAccData.phones.map((e, i) => (
               <p key={i}>
                 <span className={styles.boldText}>Contacto {i + 1}: </span>
                 {e.phone_number}
               </p>
             ))
-          ) : (
+          )/*  : (
             <p>
               <span className={styles.boldText}>Contacto 1: </span>
               {subAccData?.phones?.phone_number}
             </p>
-          )}
+          ) */}
         </div>
       </Card>
     </div>
