e54fe86c8f06604c2ee0f2d3ed0c0b66f81c71c8
Cristian Viterales <cviterales@cooperativabatan.com.ar>
2021-06-07 21:06:40 -0300

    added inputs for time start and finish

--- src/components/CloseTaskModal/CloseTaskForm/TaskFormDescription/TaskFormDescription.js
+++ src/components/CloseTaskModal/CloseTaskForm/TaskFormDescription/TaskFormDescription.js
@@ -1,29 +1,46 @@
-import React from "react"
-import styles from "./style.module.scss"
-import { useDispatch, useSelector } from "react-redux"
-import { setDescription, updateStep } from "../../../../store/actions/closeTask/closeTask"
-import Button from "../../../Button"
-import { useForm } from "react-hook-form"
-import Message from "../../../Message"
+import React, { useState } from "react";
+import styles from "./style.module.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { setDescription, updateStep, setTimeWorked } from "../../../../store/actions/closeTask/closeTask";
+import Button from "../../../Button";
+import { useForm } from "react-hook-form";
+import Message from "../../../Message";
+import InputText from "../../../InputText";
 
 const TaskFormDescription = () => {
+  const description = useSelector((state) => state.closeTask.description);
+  const dispatch = useDispatch();
+  const [error, setError] = useState(false);
+  const [inputData, setInputData] = useState({
+    start: "",
+    finish: "",
+  });
+
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm()
+  } = useForm();
+
+  const handlerInput = (e) => {
+    const isValid = /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(e.target.value);
+    if (!isValid) {
+      setError(true);
+    } else {
+      setInputData({ ...inputData, [e.target.name]: e.target.value });
+      setError(false);
+    }
+  };
 
   const onSubmit = (data) => {
-    dispatch(setDescription(data.description))
-    dispatch(updateStep("next"))
-  }
-
-  const description = useSelector((state) => state.closeTask.description)
-  const dispatch = useDispatch()
+    dispatch(setDescription(data.description));
+    dispatch(setTimeWorked(inputData.start, inputData.finish))
+    dispatch(updateStep("next"));
+  };
 
   return (
     <form className={styles.wrapper} onSubmit={handleSubmit(onSubmit)}>
-      <div>
+      <div className={styles.content}>
         <h3 className={styles.boldText}>Descripcion</h3>
         <label>
           Tarea realizada
@@ -33,9 +50,18 @@ const TaskFormDescription = () => {
             {...register("description", { required: true, minLength: 4 })}
           />
         </label>
+        <h3 className={styles.boldText}>Horas Trabajadas</h3>
+        <div className={styles.content_input}>
+          <div className={styles.input}>
+            <InputText name="start" type="time" label="Inicio" onChange={(e) => handlerInput(e)} max="24:00" required />
+          </div>
+          <div className={styles.input}>
+            <InputText name="finish" type="time" label="Fin" onChange={(e) => handlerInput(e)} max="24:00" required />
+          </div>
+        </div>
       </div>
       <div>
-        {errors.description && <Message type="error" message="Descripcion requerida" />}
+        {(errors.description || error) && <Message type="error" message="Debe completar todos los campos" />}
         <div className={styles.bottom}>
           <Button type="submit" variant="blue" onClick={() => {}}>
             Siguiente
@@ -43,7 +69,7 @@ const TaskFormDescription = () => {
         </div>
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default TaskFormDescription
+export default TaskFormDescription;
